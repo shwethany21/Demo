ac5deff68b1a82dabf0d783461fe57da3c7dbaee
shwethany21 <48521238+shwethany21@users.noreply.github.com>
2021-10-29 18:58:56 +0530

    Update LoginPageTest.java

--- src/test/java/com/freecrm/test/LoginPageTest.java
+++ src/test/java/com/freecrm/test/LoginPageTest.java
@@ -44,37 +44,4 @@ public class LoginPageTest extends BaseTest{
 		loginPage.clickOnLoginBtn();
 	}
 	
-	@Description("verify Landing Page Title")
-	@Severity(SeverityLevel.CRITICAL)
-	@Test(priority = 5 )
-	public void verifyLandingPageTitle() throws Exception {
-		loginPage.clickOnLoginBtn();
-		Thread.sleep(6000);
-		String title = landingPage.doGetLoginPageTitle();
-		System.out.println("Landing Page title is : " + title);
-		Assert.assertEquals(title, AppConstants.LANDING_PAGE_TITLE);
 	}
-	
-	@Description("Verify Forgot Password Link is dispalyed")
-	@Severity(SeverityLevel.CRITICAL)
-	@Test(priority =6 )
-	public void verifyForgotPasswordLinkIsDisplayed() throws Exception {
-		Assert.assertTrue(landingPage.forgotPasswordLinkIsDisplayed());
-	}
-
-	@Description("Verify SignUp Link is dispalyed")
-	@Severity(SeverityLevel.CRITICAL)
-	@Test(priority =7 )
-	public void verifySignUpLinkIsDisplayed() {
-		Assert.assertTrue(landingPage.signUpLinkIsDisplayed());
-	}
-	
-	@Description("verify login to app with Invalid Credentails")
-	@Severity(SeverityLevel.NORMAL)
-	@Test(priority = 8 )
-	public void verifyLoginWithInvalidCredentails() throws Exception {
-		landingPage.loginWithInvalidCredentails("test@gmail.com", "Test@1234" );
-	}
-
-
-}
